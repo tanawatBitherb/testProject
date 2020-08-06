d59f82098570831c0334b4adb0700a9fb896b434
tanawat <tanawat@bitherb.net>
2020-08-06 13:38:10 +0700

    06/08/2020_13:37

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from reservation import views, login
+from . import views
 
+app_name = 'reservation'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', login.login, name='login'),
-    path('register/', views.register, name='register'),
-    path('', include("django.contrib.auth.urls")),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('homepage/', views.homepage, name='homepage'),
+
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,18 +1,43 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
-def register(request):
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return render(request,"homepage.html",{"user":user})
+
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('posts:list')
+
+def register_view(request):
     form = UserCreationForm(request.POST)
     if form.is_valid():
         form.save()
-        return redirect('posts:list')
+        user = form.save()
+        login(request, user)
+        return render(request,"index.html")
     else:
         form = UserCreationForm()
+
     return render(request,"register.html",{"form":form})
 
-    
+
+
+def homepage(request):
+    return render(request,"homepage.html")
